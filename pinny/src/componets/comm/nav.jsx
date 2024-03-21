025302a9744bddadc1fb3f8e91c64a90bdad2c04
seonyo <jsy6535@naver.com>
2024-03-22 00:52:24 +0900

    Feat: 네비게이션 컴포넌트 생성

--- pinny/src/componets/comm/nav.jsx
+++ pinny/src/componets/comm/nav.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { Icon } from "@iconify/react";
+import "../../css/comm/index.css"
 import "../../css/comm/nav.css"
 
 const Nav = () => {
-    return(
+    return (
         <div className="nav-container">
-            <ul>
-                <li><Icon icon="ant-design:home-filled" /></li>
-                <li><Icon icon="fa-solid:coins"/></li>
-                <li><Icon icon="jam:write-f"/></li>
-                <li><img src="img/challenge.svg"></img></li>
-                <li><Icon icon="ph:user-fill"/></li>
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <Icon className="nav-icon" icon="ant-design:home-filled" />
+                    <div className="nav-label">홈</div>
+                </li>
+                <li className="nav-item">
+                    <Icon className="nav-icon" icon="fa-solid:coins" />
+                    <div className="nav-label">수입/지출</div>
+                </li>
+                <li className="nav-item">
+                    <Icon className="nav-icon" icon="jam:write-f" />
+                    <div className="nav-label">계획</div>
+                </li>
+                <li className="nav-item">
+                    <img className="nav-icon" src="img/challenge.svg"></img>
+                    <div className="nav-label">퀘스트</div>
+                </li>
+                <li className="nav-item">
+                    <Icon className="nav-icon" icon="ph:user-fill" />
+                    <div className="nav-label">마이페이지</div>
+                </li>
             </ul>
         </div>
     )
