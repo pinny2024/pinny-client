b27996c051cf6fcdf1695af884a2de3213868c52
seonyo <jsy6535@naver.com>
2024-03-24 15:53:38 +0900

    Feat: home 컴포넌트 생성

---
+++ pinny/src/componets/home/home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../css/comm/index.css"
+
+const Home = () => {
+    return (
+        <div className="plan">
+            <div className="top">
+                <div className="use-money">
+
+                </div>
+                <div className="week-plan">
+
+                </div>
+            </div>
+            <div className="career">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Nav;
