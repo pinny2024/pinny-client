48f8e51060112c5cea06d528318f8d76f907789f
GoEun <w2219@e-mirim.hs.kr>
2024-03-26 01:48:44 +0900

    Feat: home화면 구성

--- pinny/src/componets/home/home.jsx
+++ pinny/src/componets/home/home.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import { Icon } from "@iconify/react";
 import "../../css/comm/index.css"
 import "../../css/home/home.css"
 import Nav from '../../pages/nav'
+import Calendar from "react-calendar";
 
 const Home = () => {
     return (
-        <div className="plan">
+        <div className="">
             <div className="top"> 
+                수입지출기록
                 <div className="use-money">
-            
+                    <Calendar />
                 </div> 
                 <div className="week-plan">
-
+                    <div className="week-plan-name">
+                        일주일 예산을<br />세워보세요!
+                    </div>
+                    <div className="week-plan-button">
+                        <button>예산 세우기</button>
+                    </div>
                 </div>
             </div>
+            <hr />
             <div className="career">
-
+                
             </div>
             <div className="bottom-bar">
                 <Nav />
